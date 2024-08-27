55d29e9e3bdae94b59fca6bb52f85be742432b3e
Naoki427 <156777871+Naoki427@users.noreply.github.com>
2024-08-28 04:59:15 +0900

    comlete-registration fin

---
+++ app/register/sent/page.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next';
+import React from 'react';
+
+const RegisterSent: NextPage = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default RegisterSent;
